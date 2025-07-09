5372677debfcb91b17c0bbc5af665bd19872c3ba
Razvan <atomwalk12@gmail.com>
2025-07-09 14:35:47 +0200

    test: add dataset configuration file

---
+++ linalg_zero/shared.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+def get_config_dir():
+    """Get the path of the config directory"""
+    script_dir = Path(__file__).parent
+    return str(script_dir / "config")
--- tests/test_foo.py
+++
@@ -1,5 +0,0 @@
-from linalg_zero.foo import foo
-
-
-def test_foo():
-    assert foo("foo") == "foo"
