faafda63fbafee5addf7729aab6a90a5185e97f6
Razvan <atomwalk12@gmail.com>
2025-09-02 22:36:16 +0200

    chore: remove unnecessary tests

--- tests/generation/test_registry.py
+++ tests/generation/test_registry.py
@@ -1,9 +1,7 @@
-"""Tests for factory registry functionality."""
-
 import pytest
 
-from linalg_zero.generator.models import DifficultyCategory, Question, Task, Topic
-from linalg_zero.generator.registry import FactoryRegistry, create_default_registry
+from linalg_zero.generator.models import DifficultyCategory, Question, Task
+from linalg_zero.generator.registry import FactoryRegistry
 
 
 def simple_test_factory() -> Question:
@@ -84,33 +82,3 @@ def test_factory_registry_unknown_problem_type() -> None:
 
     with pytest.raises(ValueError, match="Unknown problem type"):
         registry.get_factory("math", "non-existent")
-
-
-def test_create_default_registry() -> None:
-    """Test that default registry is created with expected factories."""
-    registry = create_default_registry()
-
-    topics = registry.list_topics()
-    assert Topic.LINEAR_ALGEBRA in topics
-
-    linalg_problems = registry.list_problem_types(Topic.LINEAR_ALGEBRA)
-    assert Task.MATRIX_VECTOR_MULTIPLICATION in linalg_problems
-
-
-def test_default_registry_factories_work() -> None:
-    """Test that factories in default registry actually work."""
-    registry = create_default_registry()
-
-    mv_factory = registry.get_factory(Topic.LINEAR_ALGEBRA, Task.MATRIX_VECTOR_MULTIPLICATION)
-    question = mv_factory()
-    assert question.topic == Topic.LINEAR_ALGEBRA
-    assert len(question.answer) > 0
-
-
-def test_random_factory_selection() -> None:
-    """Test that random factory selection works."""
-    registry = create_default_registry()
-
-    random_factory = registry.get_random_factory(Topic.LINEAR_ALGEBRA)
-    question = random_factory()
-    assert question.topic == Topic.LINEAR_ALGEBRA
