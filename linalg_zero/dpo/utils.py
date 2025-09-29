c81fa1a47a2a2a0fe6bfe85ca01dc7d667fd2a19
Razvan <atomwalk12@gmail.com>
2025-09-29 07:58:24 +0200

    chore: clean-up

--- linalg_zero/dpo/utils.py
+++
@@ -1,8 +0,0 @@
-import datasets
-from linalg_zero.config.data import ScriptArguments
-from linalg_zero.distillation.utils import load_datasets_for_dpo
-
-
-def load_dataset_for_dpo(script_args: ScriptArguments) -> datasets.DatasetDict:
-    """Load the dataset for DPO."""
-    return load_datasets_for_dpo(script_args)
