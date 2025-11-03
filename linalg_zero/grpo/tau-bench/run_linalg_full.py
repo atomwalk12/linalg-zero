8f4f4b1033db6266e01dd88e766e0d605aba572f
Razvan <atomwalk12@gmail.com>
2025-11-03 14:09:02 +0100

    fix: setup scripts for full and lora training

--- linalg_zero/grpo/tau-bench/run_linalg_full.py
+++ linalg_zero/grpo/tau-bench/run_linalg_full.py
@@ -1,36 +1,29 @@
-from dotenv import load_dotenv
-
-# For debugging purposes apply dotenv file
-load_dotenv()
-
 import asyncio
 
 import art
 import hydra
 import torch
+from dotenv import load_dotenv
 from omegaconf import DictConfig, OmegaConf
 from run_rl import train
 from tau_bench.types import TauBenchPolicyConfig
 
 
-@hydra.main(version_base=None, config_path="../../config/grpo/Qwen/Qwen3-4B", config_name="config")
+@hydra.main(version_base=None, config_path="../../config/grpo/Qwen/Qwen3-4B", config_name="default")
 def main(cfg: DictConfig) -> None:
+    # Load environment variables
+    load_dotenv()
+
     # Convert all configs to plain dicts
-    init_config = OmegaConf.to_container(cfg.init, resolve=True)
     training_config = OmegaConf.to_container(cfg.training, resolve=True)
     run_config = OmegaConf.to_container(cfg.run, resolve=True)
-    peft_config = OmegaConf.to_container(cfg.peft, resolve=True)
-    trainer_args = OmegaConf.to_container(cfg.trainer, resolve=True)
     engine_args = OmegaConf.to_container(cfg.engine, resolve=True)
-    # Use peft config for LoRA training instead of:
-    # torchtune_args = OmegaConf.to_container(cfg.torchtune, resolve=True)
+    torchtune_args = OmegaConf.to_container(cfg.torchtune, resolve=True)
 
-    assert isinstance(init_config, dict), "Init config must be provided"
-    assert isinstance(training_config, dict), "Training config must be provided"
-    assert isinstance(run_config, dict), "Run config must be provided"
-    assert isinstance(peft_config, dict), "Peft args must be provided"
-    assert isinstance(trainer_args, dict), "Trainer args must be provided"
     assert isinstance(engine_args, dict), "Engine args must be provided"
+    assert isinstance(torchtune_args, dict), "Torchtune args must be provided"
+    assert isinstance(run_config, dict), "Run config must be provided"
+    assert isinstance(training_config, dict), "Training config must be provided"
 
     # Set dynamic values
     if "tensor_parallel_size" not in engine_args:
@@ -46,15 +39,10 @@ def main(cfg: DictConfig) -> None:
             run_config=run_config,
         ),
         _internal_config=art.dev.InternalModelConfig(
-            init_args=init_config,
             engine_args=engine_args,
-            trainer_args=trainer_args,
-            peft_args=peft_config,
-            # Use peft_config for LoRA training instead of:
-            # torchtune_args=torchtune_args,
+            torchtune_args=torchtune_args,
         ),
     )
-
     asyncio.run(train(model))
 
 
